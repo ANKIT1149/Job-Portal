d0beef3c1edec7f8efec6ab699c3913f87369c06
ANKIT1149 <140632363+ANKIT1149@users.noreply.github.com>
2024-11-12 20:27:13 +0530

    Hashing Password Completed

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import testRouter from "./Routes/Test.routes.js";
 import cors from "cors";
 import morgan from 'morgan';
 import Authrouter from "./Routes/Auth.routes.js";
+import ErrorMIddleware from "./Middleware/ErrorMIddleware.js";
 
 // Config dotenv & database
 dotenv.config();
@@ -19,9 +20,19 @@ app.use(express.json())
 app.use(cors());
 app.use(morgan("dev"));
 
+
 // app function
 app.use("/api/v1/test", testRouter);
 app.use("/api/v1/auth", Authrouter);
+
+// middleware declare
+app.use(ErrorMIddleware);
+
+// rest object
+
+app.get("/", (req, res) => {
+     res.send("Welcome to Job Portal")
+})
 
 // app port declare
 app.listen(process.env.PORT, () => {
